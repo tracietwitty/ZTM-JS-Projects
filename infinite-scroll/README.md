67c1d2c985fed2a6c7ac6461378f2936dc9ef59b
Tracie <traciecodes@gmail.com>
2021-01-27 13:04:35 -0600

    Completes the infinite scrolling project

--- infinite-scroll/README.md
+++ infinite-scroll/README.md
@@ -1,11 +1,14 @@
 # Explanation of the Project
 
-Fetch images from the Unsplash API and monitoring when the images are loaded in order to dynamically hide the display animation. This makes the images seem to go on indefinitely.
+This app fetches images from the Unsplash API and monitors when the images are loaded in order to dynamically hide the display animation. This way, the images seem to go on indefinitely.
 
-used a helper function to set attributes to avoid repetition in the displayPhotos() function
+
+## How I'd improve:
+
+I'd like to use the API to get specific collections to show instead of totally random pictures (i.e. nature, concerts, food, etc.). 
 
 ## Assets & Resources
-[Favicon](https://icons8.com/icons/set/infinity)
-[Unsplash API](https://unsplash.com/developers)
-[Loading Icon](https://loading.io/)
+* [Favicon](https://icons8.com/icons/set/infinity)
+* [Unsplash API](https://unsplash.com/developers)
+* [Loading Icon](https://loading.io/)
 
--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,13 +1,24 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API:
-const count = 10;
+const count = 20;
 const apiKey = '7oziyW4RyeK0ixo9z57S93ek707VIiLknq0bLQBQA6o';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Checks to see if all images were loaded:
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    } 
+}
 
 // Helper function to setAttributes on DOM elements:
 function setAttributes(element, attributes) {
@@ -18,6 +29,9 @@ function setAttributes(element, attributes) {
 
 // Creates elements for links and photos and add them to the DOM:
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+
     // Runs function for each object in photosArray:
     photosArray.forEach((photo) => {
         // Creates <a> element that links to Unsplash:
@@ -33,6 +47,9 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         })
+
+        img.addEventListener('load', imageLoaded())
+
         // Puts <img> inside <a> element and puts both inside the container element:
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -51,6 +68,14 @@ async function getPhotos() {
     }
 }
 
+
+// Loads more photos if user gets near the bottom of the page:
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+})
 
 // On load:
 getPhotos();
